58cc185658cabf4242b02c928fdf844d49c6fcc0
CHABANI Walid <chabaniw20@gmail.com>
2023-12-06 15:10:36 +0100

    Fetch Games

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area={"main"}>
-        Main
+        <GameGrid />
       </GridItem>
     </Grid>
   );
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: '0a9386a9ff1f4697b1fe82b95c4ede4b',
+  }
+});
