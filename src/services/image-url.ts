e2474fc6b25958414d7d29c0830ef927aee8eecb
CHABANI Walid <chabaniw20@gmail.com>
2023-12-12 17:32:39 +0100

    Handle Games with no Images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import NotFound from '../assets/404.png';
+
 const getCroppedImageURL = (url: string) => {
-  if (url === null) return;
+  if (url === null) return NotFound;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
