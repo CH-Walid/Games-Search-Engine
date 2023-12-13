7055a7bea6820deb28101f4ba1ad8a3082fe8bad
CHABANI Walid <chabaniw20@gmail.com>
2023-12-13 14:42:55 +0100

    Fix Long Card titles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,7 +3,7 @@ import { Card, Image, CardBody, Heading, HStack } from "@chakra-ui/react";
 import PlatformIcon from "./PlatformIcon";
 import CriticScore from "./CriticScore";
 import getCroppedImageURL from "../services/image-url";
-import Emoji from '../components/Emoji'
+import Emoji from "../components/Emoji";
 
 interface GameCardProps {
   game: Game;
@@ -11,9 +11,12 @@ interface GameCardProps {
 
 export const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card boxShadow='md' cursor='pointer' className="cardG">
-      <Image src={getCroppedImageURL(game.background_image)} objectFit='cover' />
-      <CardBody overflow='hidden'>
+    <Card boxShadow="md" cursor="pointer" className="cardG" minHeight="340px">
+      <Image
+        src={getCroppedImageURL(game.background_image)}
+        objectFit="cover"
+      />
+      <CardBody overflow="hidden">
         <HStack justifyContent="space-between" mb={2}>
           <HStack my="4px">
             {game.platforms.map(({ platform }) => {
@@ -22,7 +25,9 @@ export const GameCard = ({ game }: GameCardProps) => {
           </HStack>
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={"2xl"} >{game.name}</Heading>
+        <Heading fontSize={"2xl"}>
+          {game.name.length > 30 ? game.name.slice(0, 30) + "..." : game.name}
+        </Heading>
         <Emoji rating={game.rating_top} />
       </CardBody>
     </Card>
